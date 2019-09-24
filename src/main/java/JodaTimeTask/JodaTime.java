8fe1f2c758096d3e2602a1689817846fbc6a39ba
Yuliia Horenko <yuliia_horenko@epam.com>
2019-09-24 16:53:41 +0300

    task 6 in working

--- src/main/java/JodaTimeTask/JodaTime.java
+++ src/main/java/JodaTimeTask/JodaTime.java
@@ -26,7 +26,7 @@ public class JodaTime{
         task3(precondition(currentDate), formatter_YYYY_MM_DD, formatter_DD_MM_YYYY);
         task4(precondition(currentDate), formatter_YYYY_MM_DD, longFormatter);
         task5(precondition(currentDate), formatter_YYYY_MM_DD, longFormatter);
-        task6(precondition(currentDate), DateTimeFormat.forPattern("yyyy-MM-dd.HH.mm.ss.SS"), longFormatter);
+        task6(DateTimeFormat.forPattern("yyyy-MM-dd.HH.mm.ss.SS"));
 //        task7(precondition(currentDate), formatter_YYYY_MM_DD, longFormatter);
         task8(DateTime.parse(precondition(currentDate)));
 
@@ -61,8 +61,8 @@ public class JodaTime{
         return outputFormatter.print(inputFormatter.parseDateTime(date).millisOfDay().withMaximumValue().getMillis());
     }
 
-    private static String task6(String date, DateTimeFormatter longFormatter, DateTimeFormatter outputFormatter) {
-        return outputFormatter.print(longFormatter.parseDateTime(date).millisOfDay().withMaximumValue().getMillis());
+    private static String task6(DateTimeFormatter date) {
+        return String.valueOf(date) ;
     }
 
     private static String task8(DateTime currentDate) {
