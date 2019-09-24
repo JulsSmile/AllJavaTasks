b13f23a8c8e0e7b86b0c75b19c398c6a1e6a9077
Yuliia Horenko <yuliia_horenko@epam.com>
2019-09-24 16:20:31 +0300

    task 8 done

--- src/main/java/JodaTimeTask/JodaTime.java
+++ src/main/java/JodaTimeTask/JodaTime.java
@@ -14,57 +14,54 @@ public class JodaTime{
     private static DateTimeFormatter longFormatter = DateTimeFormat.forPattern("yyyy-MM-dd.HH.mm.ss.SS");
 
 
-
-
     public static void main(String[] args) {
 
         DateTime currentDate = new DateTime();
         System.out.println("Current date: " + currentDate);
         String input = precondition(currentDate);
-        task1(precondition(currentDate),formatter_YYYY_MM_DD,formatter_MM_DD_YYYY);
-        task2(precondition(currentDate),formatter_YYYY_MM_DD,formatter_DD_MM_YYYY);
-        task3(precondition(currentDate),formatter_YYYY_MM_DD,formatter_DD_MM_YYYY);
-        task4(precondition(currentDate),formatter_YYYY_MM_DD,longFormatter);
-        task5(precondition(currentDate),formatter_YYYY_MM_DD,longFormatter);
-        task6(precondition(currentDate),formatter_YYYY_MM_DD,longFormatter);
+        task1(precondition(currentDate), formatter_YYYY_MM_DD, formatter_MM_DD_YYYY);
+        task2(precondition(currentDate), formatter_YYYY_MM_DD, formatter_DD_MM_YYYY);
+        task3(precondition(currentDate), formatter_YYYY_MM_DD, formatter_DD_MM_YYYY);
+        task4(precondition(currentDate), formatter_YYYY_MM_DD, longFormatter);
+        task5(precondition(currentDate), formatter_YYYY_MM_DD, longFormatter);
+        task6(precondition(currentDate), formatter_YYYY_MM_DD, longFormatter);
         task8(DateTime.parse(precondition(currentDate)));
 //        task7(precondition(currentDate),formatter_YYYY_MM_DD,longFormatter);
-
-
-
 
         LocalDate dateForCompare = new LocalDate(968498900000L);
         String dateInFutureFormatted = formatter_DD_MM_YYYY.print(dateForCompare);
 //        boolean isBefore = currentDate.isBefore(dateForCompare);
 //        System.out.println(currentDate + " is before " + dateInFutureFormatted + " : " + isBefore);
-
-        DayOfWeek dayOfWeek = DayOfWeek.of(currentDate.getDayOfWeek());
-        System.out.println("Current day of week is " + dayOfWeek);
     }
 
     private static String precondition(DateTime date) {
         return formatter_YYYY_MM_DD.print(date);
     }
 
-
     private static String task1(String date, DateTimeFormatter inputFormatter, DateTimeFormatter outputFormatter) {
         return outputFormatter.print(inputFormatter.parseDateTime(date));
     }
+
     private static String task2(String date, DateTimeFormatter inputFormatter, DateTimeFormatter outputFormatter) {
         return outputFormatter.print(inputFormatter.parseDateTime(date));
     }
+
     private static String task3(String date, DateTimeFormatter inputFormatter, DateTimeFormatter outputFormatter) {
         return outputFormatter.print(inputFormatter.parseDateTime(date));
     }
+
     private static String task4(String date, DateTimeFormatter inputFormatter, DateTimeFormatter outputFormatter) {
         return outputFormatter.print(inputFormatter.parseDateTime(date).withTimeAtStartOfDay().getMillis());
     }
+
     private static String task5(String date, DateTimeFormatter inputFormatter, DateTimeFormatter outputFormatter) {
         return outputFormatter.print(inputFormatter.parseDateTime(date).millisOfDay().withMaximumValue().getMillis());
     }
+
     private static String task6(String date, DateTimeFormatter inputFormatter, DateTimeFormatter outputFormatter) {
         return outputFormatter.print(inputFormatter.parseDateTime(date).millisOfDay().withMaximumValue().getMillis());
     }
+
     private static String task8(DateTime currentDate) {
         return String.valueOf(DayOfWeek.of(currentDate.getDayOfWeek()));
     }
